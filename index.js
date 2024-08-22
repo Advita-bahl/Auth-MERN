747557e2911e7bc60a28119fdf0faec4c439dbf1
Advita Bahl <21JE0063@iitism.ac.in>
2024-08-22 10:41:11 +0530

    changes

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ const express= require("express");
 const app= express();
 const mongoose = require("mongoose");
 const dotenv= require("dotenv");
+const path = require("path");
 dotenv.config();
 
 const cors= require("cors");
 
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
 
 const userRoute =require("./routes/userRoute.js");
 
-app.use(express.json());
+const _filename = __filename;
+const _dirname= path.dirname(_filename);
+console.log(_dirname);
 
-mongoose.connect(process.env.URI)
+app.use(express.static(path.join(_dirname, '/frontend/dist')));
+
+
+
+mongoose.connect(process.env.URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log("connected successfully");
     app.listen(port , (err)=>{
